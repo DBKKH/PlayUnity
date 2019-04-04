c19291c3c15be6635ca85a964caa02aa8aa7cbd8
hdkkik <hdkkik@gmail.com>
2019-04-04 15:21:27 +0900

    画像を追加しIconを変更，アプリ名を変更，重複起動用のクラスを作成

---
+++ Assets/MyScripts/AvoidDoubleEXE.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class AvoidDoubleEXE : MonoBehaviour
+{
+	public Button endButton;
+	public Text launchDoubleAppLog;
+	public string errorLog = "がすでにが起動しています";
+	private void Start()
+	{
+		launchDoubleAppLog.gameObject.SetActive(false);
+		endButton.gameObject.SetActive(false);
+
+		var appName = Application.productName;
+		errorLog += appName;
+		launchDoubleAppLog.text = errorLog;
+
+		if (System.Diagnostics.Process.GetProcessesByName(appName).GetUpperBound(0) > 0)
+		{
+			launchDoubleAppLog.gameObject.SetActive(true);
+			endButton.gameObject.SetActive(true);
+
+			Debug.Log(errorLog);
+			endButton.onClick.AddListener(()=>Application.Quit());			
+		}
+	}
+}
