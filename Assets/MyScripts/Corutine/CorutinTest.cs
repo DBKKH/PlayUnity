e413bb7bf7254e1bea96a3a6ce6b3ce1b10db49c
DBKKH <hdkkik@gmail.com>
2019-06-22 03:50:43 +0900

    ?

---
+++ Assets/MyScripts/Corutine/CorutinTest.cs
@@ -0,0 +1,27 @@
+﻿//https://doruby.jp/users/ino/entries/%E3%80%90C--Unity%E3%80%91%E3%82%B3%E3%83%AB%E3%83%BC%E3%83%81%E3%83%B3(Coroutine)%E3%81%A8%E3%81%AF%E4%BD%95%E3%81%AA%E3%81%AE%E3%81%8B
+
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class CorutinTest : MonoBehaviour
+{
+    public Text outText;
+    public float waitSecond;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        StartCoroutine(nameof(Corutiner));
+    }
+
+IEnumerator Corutiner(){
+    outText.text = "first ";
+    if (waitSecond>0) yield return new WaitForSecondsRealtime(waitSecond);
+    else yield return null;
+
+    outText.text += "second ";
+}
+
+}
---
+++ Assets/MyScripts/Corutine/StopCorutin.cs
@@ -0,0 +1,41 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class StopCorutin : MonoBehaviour 
+{
+	[SerializeField] UnityEngine.UI.Text text;
+	[SerializeField] float waitTime = 3;
+ float clickTime = 0;
+
+	IEnumerator coroutineMethod;
+	
+	void Start()
+	{
+		// IEnumeratorを取得する
+		coroutineMethod = ChangeColorCoroutine();
+	}
+	
+	void OnMouseDown()
+	{
+		// コルーチンをIEnumeratorの位置から開始（再開）
+		StartCoroutine(coroutineMethod);
+	}
+	
+	void OnMouseUp()
+	{
+		// コルーチンを停止
+		StopCoroutine(coroutineMethod);
+	}
+	
+	IEnumerator ChangeColorCoroutine()
+	{
+		// コルーチン進行中は数値を増加させる
+		while( clickTime < waitTime ){
+			clickTime += Time.deltaTime;
+			text.text = clickTime.ToString("0.000");
+			yield return null;
+		}
+		text.text = "finished";
+	}
+}
