26ef60f1794737477900e0b84e6089f2c7c4c3a3
hideki.koike <hdkkik@gmail.com>
2020-11-10 18:48:59 +0900

    make scene to test get argument from CLI

---
+++ Assets/MyScripts/TestGetArgs.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using UnityEngine;
+using UnityEngine.UI;
+
+[RequireComponent(typeof(Text))]
+public class TestGetArgs : MonoBehaviour
+{
+    private Text argText;
+    private string resultArgs = "None";
+
+    private void Awake()
+    {
+        argText = GetComponent<Text>();
+    }
+
+    private void Start()
+    {
+#if UNITY_STANDALONE_WIN
+        resultArgs = "";
+        var args = Environment.GetCommandLineArgs();
+
+        if (args.Length >= 10)
+        {
+            argText.text += "Maximam = 10 args";
+            return;
+        }
+
+        int i = 0;
+        while (i < args.Length)
+        {
+            resultArgs += i.ToString();
+            resultArgs += ":";
+            resultArgs += args[i];
+            resultArgs += ", ";
+        }
+#endif
+
+        argText.text += resultArgs;
+    }
+}
